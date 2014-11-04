12232e0b569bc0d5b2c94758fbbb34be48d017c7
allex32 <matlashallex32@gmail.com>
2014-11-04 05:32:37 +0300

    reformatting code, remove .idea

--- audio_consumer.js
+++ audio_consumer.js
@@ -1,37 +1,34 @@
 /**
-Модуль, отвечающий за принятие task-ов из очереди
+ Модуль, отвечающий за принятие task-ов из очереди
 
  */
 
 
-var kue=require('kue'),
+var kue = require('kue'),
     jobs = kue.createQueue();
 
 
 var speech_recognition = require('./speech_recognition');
 
-jobs.process('audio',function(job,done){
+jobs.process('audio', function (job, done) {
 
-        var result  = speech_recognition(job.data,getResponse);
+    var result = speech_recognition(job.data, getResponse);
 
-        function getResponse(err,httpResponse,xml,outFile){
-            if(err) {
-                console.log('ERROR ' +err);
-                done(err);
-
-            }
-            else {
-                if(outFile)
-                console.log('response is written in '+outFile+ ' file');
-                else
-                console.log(httpResponse.statusCode, xml);
-
-                done();
-            }
+    function getResponse(err, httpResponse, xml, outFile) {
+        if (err) {
+            console.log('ERROR ' + err);
+            done(err);
         }
 
+        else {
+            if (outFile)
+                console.log('response is written in ' + outFile + ' file');
+            else
+                console.log(httpResponse.statusCode, xml);
 
-
+            done();
+        }
+    }
 });
 
 
--- speech_recognition.js
+++ speech_recognition.js
@@ -15,75 +15,71 @@ var fs = require('fs');
 //для использование через yandex-api
 
 //конфигурирует параметры для yandex-сервера(функции подобного рода можно вынести в отдельный модул)
-var yandex_opt_config = function(data,callback){
+var yandex_opt_config = function (data, callback) {
 
     var result = {};
-    for(var key in new_opts.yandex){
+    for (var key in new_opts.yandex) {
         result[new_opts.yandex[key]] = data[key];
     }
-    result.developer_key=keys.yandex;
-    callback(null,result);
+    result.developer_key = keys.yandex;
+    callback(null, result);
 };
 
 
-
-
-
-
-
-var speech_recognition_response = function(data,callback){
+var speech_recognition_response = function (data, callback) {
 
     var self_callback = callback;
 
-   if(data.service=='yandex'){
-       //приводим опции к виду, "скармливаемому" yandex-api
-       yandex_opt_config(data,returnData);
+    if (data.service == 'yandex') {
+        //приводим опции к виду, "скармливаемому" yandex-api
+        yandex_opt_config(data, returnData);
 
-        function returnData(err,result){
-            if(err)
-            self_callback(err);
+        function returnData(err, result) {
+            if (err)
+                self_callback(err);
+
             //используем функцию из модуля, которая непосредственно отправляет запрос
             //к серверам yandex-а
-            yandex_speech.ASR(result,yandex_response);
 
-            function yandex_response(err,httpResponse,xml){
+            yandex_speech.ASR(result, yandex_response);
+
+            function yandex_response(err, httpResponse, xml) {
                 //отлов внутренних ошибок модуля
-                if(err)
-                self_callback(err);
+
+                if (err)
+                    self_callback(err);
+
                 //если пришел ответ, но не успешный, то отправляем код и тело ответа
-                if(httpResponse.statusCode!=200) {
+
+                if (httpResponse.statusCode != 200) {
                     var response = httpResponse.statusCode + ' ' + httpResponse.body;
                     self_callback(response);
                 }
+
                 //иначе, в данном случае, как пример, записываем ответ в файл
-                else{
+                else {
                     var tmp = data.file.toString().lastIndexOf('.');
+                    var outFile = data.file.substring(0, tmp) + '.xml';
 
-                    var outFile = data.file.substring(0,tmp)+'.xml';
+                    fs.writeFile(outFile, xml, function (err) {
+                        if (err)
+                            self_callback(err);
 
-
-                    fs.writeFile(outFile,xml,function(err){
-                        if(err) self_callback(err);
-
-                        self_callback(null,httpResponse,xml,outFile);
+                        self_callback(null, httpResponse, xml, outFile);
 
                     });
-
-
                 }
-
-
             }
         }
 
-   }
+    }
 
     //аналогично для гугла, еще не дописано
-    if(data.service=='google'){
+    if (data.service == 'google') {
 
-       callback(null,name_serivce);
+        callback(null, name_serivce);
 
-   }
+    }
 
 
     //если ни один из параметров не подходит
